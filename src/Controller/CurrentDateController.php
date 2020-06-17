68948433fb54cf7242f3697944595200de2753f9
Damian Cybiak <d.cybiak@softwizard.pl>
2020-06-17 20:52:16 +0200

    Create function moveToAction. Little changes

--- src/Controller/CurrentDateController.php
+++ src/Controller/CurrentDateController.php
@@ -2,23 +2,22 @@
 
 namespace App\Controller;
 
-use DateTime;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CurrentDateController
 {
-//    /**
-//     * jeśli zainstalujemy symfony require annotations, wtedy możemy ustawiać ściężkę tutaj zamiast w routingach
-//     * ścieżka będzie działać zarówna ta jak i ta w routes.yaml
-//     * @Route(path="/index")
-//     * @return Response
-//     * @throws
-//    **/
-    public function main(): Response
+    /**
+     * jeśli zainstalujemy symfony require annotations, wtedy możemy ustawiać ściężkę tutaj zamiast w routingach
+     * ścieżka będzie działać zarówna ta jak i ta w routes.yaml
+     * @Route(path="/index", name="currentDate")
+     * @return Response
+     * @throws
+    **/
+    public function currentDate(): Response
     {
-        $currentDate = new DateTime(); //$currentDate jest obiektem klasy DateTime
+        $currentDate = new \DateTime(); //$currentDate jest obiektem klasy DateTime
 
-        return new Response("<html><body>" . $currentDate->format(DATE_ATOM) . "</body>"); //dlatego trzeba go przerobić na stringa
+        return new Response("<html><body>" . $currentDate->format(DATE_ATOM) . "</body></html>");
     }
 }
--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,24 +4,42 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     /**
      * @param string $name
-     * @Route(path="/hi/{name}")
+     * @Route(path="/hi/{name}", name="hello")
      * @param Request $request
      * @return Response
      */
-    public function hi(string $name, Request $request): Response
+    public function hello(string $name, Request $request): Response
     {
-        $helloText = "Cześc " . $name;
+        $helloText = "Cześć " . $name;
         $param1 = $request->get('param1');
         return new Response("<html><body><h1>$helloText</h1><h2>$param1</h2></body></html>");
     }
 
+    /**
+     * @Route(path="redirect/{action}")
+     * @param string $action
+     * @return RedirectResponse
+     * @throws \Exception
+     */
+    public function moveToAction(string $action): RedirectResponse
+    {
+
+        if('hello' == $action){
+            return $this->redirectToRoute('hello', ['name' => 'Some name']);
+        }else if ('currentDate' == $action){
+            return $this->redirectToRoute('currentDate');
+        }
+        throw new \Exception("Wrong action");
+    }
 
 }
