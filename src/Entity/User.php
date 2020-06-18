8e02e6d1668bd9c1a0f7990f693caf1fb1af2253
Damian Cybiak <d.cybiak@softwizard.pl>
2020-06-18 19:53:17 +0200

    add user Entity

---
+++ src/Entity/User.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @package App\Entity
+ * @ORM\Entity()
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $name;
+
+    /**
+     * nullable na true mówi o tym, że nazwisko może być nullem
+     * @ORM\Column(type="string", nullable=true)
+     * @var string|null
+     */
+    private $surrname;
+
+    /**
+     * User constructor.
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $surrname
+     */
+    public function setSurrname(string $surrname): void
+    {
+        $this->surrname = $surrname;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSurrname(): ?string
+    {
+        return $this->surrname;
+    }
+
+
+
+}
